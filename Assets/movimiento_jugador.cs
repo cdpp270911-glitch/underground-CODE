76aec5daf42b53a718fb6f7e41b6b7ea510f0555
cristian <cdpp270911@gmail.com>
2026-02-28 12:14:26 -0500

    Primer commit

---
+++ Assets/movimiento_jugador.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento_jugador : MonoBehaviour
+{   bool enSuelo; 
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {   
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Saltar(); 
+    }
+    private void Saltar()
+    {
+        if (Input.GetButtonDown("Jump") && enSuelo ) 
+        {
+            rb.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+            
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("piso"))
+        {
+            Debug.Log("toco el piso");
+            enSuelo = true;
+        }
+        
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("piso"))
+        {
+            Debug.Log("toco el piso");
+            enSuelo = false;
+        }
+}
+}
